bb427a3fefb9046911345f30e4055e62757fb964
zaz7 <abdullah75farid@gmail.com>
2024-07-11 22:55:48 +0300

    eee

--- project-2.js
+++ project-2.js
@@ -12,3 +12,7 @@ let closeBtn = document.getElementById("close-btn");
 openBtn.addEventListener("click", function(){
     modalContainer.style.display="block";
 })
+
+closeBtn.addEventListener("click",function(){
+    modalContainer.style.display = "none";
+})
