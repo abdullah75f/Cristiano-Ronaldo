fdec954056413cbba5dade5f9913655dfe6c38fc
zaz7 <abdullah75farid@gmail.com>
2024-07-11 22:47:27 +0300

    lkkk

--- project-2.js
+++ project-2.js
@@ -5,3 +5,10 @@ let openBtn = document.getElementById("open-btn");
 let modalContainer = document.getElementById("modal-container");
 
 let closeBtn = document.getElementById("close-btn");
+
+
+
+//Event listeners
+openBtn.addEventListener("click", function(){
+    
+})
