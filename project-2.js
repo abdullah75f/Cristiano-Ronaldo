f4285068ff3ebbf1f447f37e4a4ae533e51aabc2
zaz7 <abdullah75farid@gmail.com>
2024-07-11 22:41:11 +0300

    ddvefv

---
+++ project-2.js
@@ -0,0 +1,7 @@
+//Variables
+
+let openBtn = document.getElementById("open-btn");
+
+let modalContainer = document.getElementById("modal-container");
+
+let closeBtn = document.getElementById("close-btn");
